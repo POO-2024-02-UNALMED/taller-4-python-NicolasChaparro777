154dc3388c06f9557cf0033a300554750c26e280
NicolasChaparro777 <jchaparroro@unal.edu.co>
2024-11-22 10:07:11 -0500

    intento3

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,14 +3,14 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes = None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes = None):
         self._grupo = grupo
-        if asignaturas == None:
+        if asignaturas is None:
             self._asignaturas = []
         else:
             self._asignaturas = asignaturas
         
-        if estudiantes == None:
+        if estudiantes is None:
             self.listadoAlumnos = []
         else:
             self.listadoAlumnos = estudiantes
@@ -20,7 +20,7 @@ class Grupo:
             self._asignaturas.append(Asignatura(value))
 
     def agregarAlumno(self, alumno, lista=None):
-        if lista == None:
+        if lista is None:
             lista = []
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
